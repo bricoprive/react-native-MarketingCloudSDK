8b3a67ab9eac6ad4e85148b79f3b7e1331c88f3d
Daniel Ferro <dlcerva@gmail.com>
2018-09-07 17:24:09 -0400

    Updated package name

--- android/src/main/java/com/danielferro/marketingcloudsdk/RNMarketingCloudSDKModule.java
+++ android/src/main/java/com/danielferro/marketingcloudsdk/RNMarketingCloudSDKModule.java
@@ -36,7 +36,7 @@ public class RNMarketingCloudSDKModule extends ReactContextBaseJavaModule {
 
   @Override
   public String getName() {
-    return "MarketingCloudSdk";
+    return "MarketingCloudSDK";
   }
 
   /*
